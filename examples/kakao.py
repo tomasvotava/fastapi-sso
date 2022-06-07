283ce20a25588794efddc8ffbe2c65dbe6fc2ade
thdwoqor <celebarte01@gmail.com>
2022-06-07 10:05:20 +0900

    feat: add kakao examples

---
+++ examples/kakao.py
@@ -0,0 +1,37 @@
+"""Kakao Login Example
+"""
+
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi_sso.sso.kakao import KakaoSSO
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+app = FastAPI()
+
+sso = KakaoSSO(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri="http://localhost:5000/auth/callback",
+    allow_insecure_http=True,
+    use_state=False,
+)
+
+
+@app.get("/auth/login")
+async def auth_init():
+    """Initialize auth and redirect"""
+    return await sso.get_login_redirect()
+
+
+@app.get("/auth/callback")
+async def auth_callback(request: Request):
+    """Verify login"""
+    return await sso.verify_and_process(request, params={"client_secret": CLIENT_SECRET})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="examples.kakao:app", host="127.0.0.1", port=5000, reload=True)
