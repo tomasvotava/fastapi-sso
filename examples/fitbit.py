5dd9b1f0bf5c83cf2fb8e9d8a522c2eeac0e0466
Tomas Votava <tomas.votava@bizztreat.com>
2022-06-20 07:36:08 +0200

    feat: fitbit login support

---
+++ examples/fitbit.py
@@ -0,0 +1,37 @@
+"""Fitbit Login Example
+"""
+
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi_sso.sso.fitbit import FitbitSSO
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+app = FastAPI()
+
+sso = FitbitSSO(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri="http://localhost:3000/auth/callback",
+    allow_insecure_http=True,
+    use_state=True,
+)
+
+
+@app.get("/auth/login")
+async def auth_init():
+    """Initialize auth and redirect"""
+    return await sso.get_login_redirect()
+
+
+@app.get("/auth/callback")
+async def auth_callback(request: Request):
+    """Verify login"""
+    return await sso.verify_and_process(request)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="examples.fitbit:app", host="127.0.0.1", port=3000)
---
+++ fastapi_sso/sso/fitbit.py
@@ -0,0 +1,37 @@
+"""Fitbit OAuth Login Helper
+"""
+
+from typing import Dict
+
+from fastapi_sso.sso.base import OpenID, SSOBase, SSOLoginError
+
+
+class FitbitSSO(SSOBase):
+    """Class providing login via Fitbit OAuth"""
+
+    provider = "fitbit"
+    scope = ["profile"]
+
+    @classmethod
+    async def openid_from_response(cls, response: dict) -> OpenID:
+        """Return OpenID from user information provided by Google"""
+        info = response.get("user")
+        if not info:
+            raise SSOLoginError(401, "Failed to process login via Fitbit")
+        return OpenID(
+            id=info["encodedId"],
+            first_name=info["fullName"],
+            display_name=info["displayName"],
+            picture=info["avatar"],
+            email=None,
+            last_name=None,
+            provider=cls.provider,
+        )
+
+    async def get_discovery_document(self) -> Dict[str, str]:
+        """Get document containing handy urls"""
+        return {
+            "authorization_endpoint": "https://www.fitbit.com/oauth2/authorize?response_type=code",
+            "token_endpoint": "https://api.fitbit.com/oauth2/token",
+            "userinfo_endpoint": "https://api.fitbit.com/1/user/-/profile.json",
+        }
