e69844b18c0609cab248e4c474121e42087398b3
1tang2bang92 <shpark0906@naver.com>
2023-01-26 19:46:11 +0900

    add naver sso

---
+++ fastapi_sso/sso/naver.py
@@ -0,0 +1,23 @@
+"""Naver SSO Oauth Helper class"""
+
+
+from fastapi_sso.sso.base import DiscoveryDocument, OpenID, SSOBase
+
+
+class NaverSSO(SSOBase):
+    """Class providing login using Naver OAuth"""
+
+    provider = "naver"
+    scope = []
+    additional_headers = {"accept": "application/json"}
+
+    async def get_discovery_document(self) -> DiscoveryDocument:
+        return {
+            "authorization_endpoint": "https://nid.naver.com/oauth2.0/authorize",
+            "token_endpoint": "https://nid.naver.com/oauth2.0/token",
+            "userinfo_endpoint": f"https://openapi.naver.com/v1/nid/me",
+        }
+
+    @classmethod
+    async def openid_from_response(cls, response: dict) -> OpenID:
+        return OpenID(display_name=response["properties"]["nickname"], provider=cls.provider)
