5a0239c691a5c333a17b136d7fb5b4d8cc7fd58d
Alessandro Pischedda <alessandro.pischedda@gmail.com>
2023-06-25 22:00:52 +0200

    Add Gitlab SSO

---
+++ fastapi_sso/sso/gitlab.py
@@ -0,0 +1,28 @@
+"""Gitlab SSO Oauth Helper class"""
+
+from fastapi_sso.sso.base import DiscoveryDocument, OpenID, SSOBase
+
+
+class GitlabSSO(SSOBase):
+    """Class providing login via Gitlab SSO"""
+
+    provider = "gitlab"
+    scope = ["read_user"]
+    additional_headers = {"accept": "application/json"}
+
+    async def get_discovery_document(self) -> DiscoveryDocument:
+        return {
+            "authorization_endpoint": "https://gitlab.com/oauth/authorize",
+            "token_endpoint": "https://gitlab.com/oauth/token",
+            "userinfo_endpoint": "https://gitlab.com/api/v4/user",
+        }
+
+    @classmethod
+    async def openid_from_response(cls, response: dict) -> OpenID:
+        return OpenID(
+            email=response["email"],
+            provider=cls.provider,
+            id=response["id"],
+            display_name=response["login"],
+            picture=response["avatar_url"],
+        )
