8a472782f3c72fbf3d67cab9e769be39a0ad2277
Tomas Votava <info@tomasvotava.eu>
2023-09-29 19:19:12 +0200

    fix: remove commented code

--- docs/generate_reference.py
+++ docs/generate_reference.py
@@ -62,34 +62,3 @@ def on_config(config: "mkdocs.config.defaults.MkDocsConfig"):
     """Generate reference pages for the documentation."""
     generate_example_pages(config.docs_dir, config.nav)
     generate_reference_pages(config.docs_dir, config.nav)
-
-
-# from pathlib import Path
-
-# import mkdocs_gen_files
-
-# nav = mkdocs_gen_files.Nav()
-
-# for path in sorted(Path("./fastapi_sso").rglob("*.py")):
-#     module_path = path.relative_to(".").with_suffix("")
-#     doc_path = path.relative_to("./fastapi_sso/").with_suffix(".md")
-#     full_doc_path = Path("./reference/", doc_path)
-
-#     parts = list(module_path.parts)
-
-#     if parts[-1] == "__init__":
-#         parts = parts[:-1]
-#     elif parts[-1] == "__main__":
-#         continue
-
-#     nav[parts] = Path("./reference", doc_path).as_posix()
-
-#     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-#         identifier = ".".join(parts)
-#         print("::: " + identifier, file=fd)
-#         print(identifier, parts, doc_path, full_doc_path)
-
-#     mkdocs_gen_files.set_edit_path(full_doc_path, path)
-
-# with mkdocs_gen_files.open("reference.md", "w") as nav_file:
-#     nav_file.writelines(nav.build_literate_nav())
