73469d9e3916a0d13f3880f96bac391da5781ba3
Jimmy Yeh <chienfeng0719@gmail.com>
2024-02-06 17:46:21 +0800

    Add line in tests

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -16,6 +16,7 @@ from fastapi_sso.sso.github import GithubSSO
 from fastapi_sso.sso.gitlab import GitlabSSO
 from fastapi_sso.sso.google import GoogleSSO
 from fastapi_sso.sso.kakao import KakaoSSO
+from fastapi_sso.sso.line import LineSSO
 from fastapi_sso.sso.microsoft import MicrosoftSSO
 from fastapi_sso.sso.naver import NaverSSO
 from fastapi_sso.sso.spotify import SpotifySSO
@@ -38,6 +39,7 @@ tested_providers = (
     GitlabSSO,
     GoogleSSO,
     KakaoSSO,
+    LineSSO,
     MicrosoftSSO,
     NaverSSO,
     SpotifySSO,
