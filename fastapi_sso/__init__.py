40edce51d864688a2f0360300eae0e5c1f87e9f8
Jimmy Yeh <42854594+jimmyyyeh@users.noreply.github.com>
2024-02-06 17:52:59 +0800

    Add line sso (#124)
    * Add line sso

--- fastapi_sso/__init__.py
+++ fastapi_sso/__init__.py
@@ -10,6 +10,7 @@ from .sso.github import GithubSSO
 from .sso.gitlab import GitlabSSO
 from .sso.google import GoogleSSO
 from .sso.kakao import KakaoSSO
+from .sso.line import LineSSO
 from .sso.microsoft import MicrosoftSSO
 from .sso.naver import NaverSSO
 from .sso.notion import NotionSSO
