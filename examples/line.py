74d2836bb1954ac0fbd174ab0c1d4b91acfde2e1
Jimmy Yeh <42854594+jimmyyyeh@users.noreply.github.com>
2024-02-06 17:52:59 +0800

    Add line sso (#124)
    * Add line sso

---
+++ examples/line.py
@@ -0,0 +1,37 @@
+"""Line Login Example
+"""
+
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi_sso.sso.line import LineSSO
+
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+
+app = FastAPI()
+
+sso = LineSSO(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri="http://localhost:5000/auth/callback",
+    allow_insecure_http=True,
+)
+
+
+@app.get("/auth/login")
+async def auth_init():
+    """Initialize auth and redirect"""
+    with sso:
+        return await sso.get_login_redirect(state="randomstate")
+
+@app.get("/auth/callback")
+async def auth_callback(request: Request):
+    """Verify login"""
+    with sso:
+        user = await sso.verify_and_process(request)
+    return user
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="examples.line:app", host="127.0.0.1", port=5000)
--- fastapi_sso/__init__.py
+++ fastapi_sso/__init__.py
@@ -10,6 +10,7 @@ from .sso.github import GithubSSO
 from .sso.gitlab import GitlabSSO
 from .sso.google import GoogleSSO
 from .sso.kakao import KakaoSSO
+from .sso.line import LineSSO
 from .sso.linkedin import LinkedInSSO
 from .sso.microsoft import MicrosoftSSO
 from .sso.naver import NaverSSO
