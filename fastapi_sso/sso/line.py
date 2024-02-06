830e26e5a8f4a403fce0073f2b9414b028b52dde
Jimmy Yeh <chienfeng0719@gmail.com>
2024-02-06 17:12:03 +0800

    Line using double quotes

--- fastapi_sso/sso/line.py
+++ fastapi_sso/sso/line.py
@@ -11,29 +11,28 @@ if TYPE_CHECKING:
 
 
 class LineSSO(SSOBase):
-    '''Class providing login via Line OAuth'''
+    """Class providing login via Line OAuth"""
 
-    provider = 'line'
-    base_url = 'https://api.line.me/oauth2/v2.1'
-    scope = ['email', 'profile', 'openid']
+    provider = "line"
+    base_url = "https://api.line.me/oauth2/v2.1"
+    scope = ["email", "profile", "openid"]
 
     async def get_discovery_document(self) -> DiscoveryDocument:
-        '''Get document containing handy urls'''
+        """Get document containing handy urls"""
         return {
-            'authorization_endpoint': f'https://access.line.me/oauth2/v2.1/authorize',
-            'token_endpoint': f'{self.base_url}/token',
-            'userinfo_endpoint': f'{self.base_url}/userinfo'
+            "authorization_endpoint": f"https://access.line.me/oauth2/v2.1/authorize",
+            "token_endpoint": f"{self.base_url}/token",
+            "userinfo_endpoint": f"{self.base_url}/userinfo",
         }
 
-    async def openid_from_response(self, response: dict, session: Optional['httpx.AsyncClient'] = None) -> OpenID:
-        '''Return OpenID from user information provided by Line'''
+    async def openid_from_response(self, response: dict, session: Optional["httpx.AsyncClient"] = None) -> OpenID:
+        """Return OpenID from user information provided by Line"""
         return OpenID(
-            email=response.get('email'),
-            first_name='',
-            last_name='',
-            display_name=response.get('name'),
+            email=response.get("email"),
+            first_name="",
+            last_name="",
+            display_name=response.get("name"),
             provider=self.provider,
-            id=response.get('sub'),
-            picture=response.get('picture'),
+            id=response.get("sub"),
+            picture=response.get("picture"),
         )
-
