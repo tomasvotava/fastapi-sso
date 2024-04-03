d5918b4ccd16f9a1ef0482f1b9810e13885cdf5c
akimrx <akimstrong@yandex.ru>
2024-04-03 09:30:11 +0300

    fix(gitlab): updated first/last name parsing, fixed style

--- fastapi_sso/sso/gitlab.py
+++ fastapi_sso/sso/gitlab.py
@@ -1,6 +1,7 @@
 """Gitlab SSO Oauth Helper class"""
 
-from typing import TYPE_CHECKING, Any, List, Optional, Union
+from typing import TYPE_CHECKING, List, Optional, Tuple, Union
+from urllib.parse import urljoin
 
 import pydantic
 
@@ -22,11 +23,11 @@ class GitlabSSO(SSOBase):
         self,
         client_id: str,
         client_secret: str,
-        base_endpoint_url: Optional[Union[pydantic.AnyHttpUrl, str]] = None,
-        redirect_uri: Any | str | None = None,
+        redirect_uri: Optional[Union[pydantic.AnyHttpUrl, str]] = None,
         allow_insecure_http: bool = False,
         use_state: bool = False,  # TODO: Remove use_state argument
-        scope: List[str] | None = None,
+        scope: Optional[List[str]] = None,
+        base_endpoint_url: Optional[str] = None,
     ) -> None:
         super().__init__(
             client_id,
@@ -39,17 +40,38 @@ class GitlabSSO(SSOBase):
         self.base_endpoint_url = base_endpoint_url or self.base_endpoint_url
 
     async def get_discovery_document(self) -> DiscoveryDocument:
+        """Override the discovery document method to return Yandex OAuth endpoints."""
+
         return {
-            "authorization_endpoint": f"{self.base_endpoint_url}/oauth/authorize",
-            "token_endpoint": f"{self.base_endpoint_url}/oauth/token",
-            "userinfo_endpoint": f"{self.base_endpoint_url}/api/v4/user",
+            "authorization_endpoint": urljoin(self.base_endpoint_url, "/oauth/authorize"),
+            "token_endpoint": urljoin(self.base_endpoint_url, "/oauth/token"),
+            "userinfo_endpoint": urljoin(self.base_endpoint_url, "/api/v4/user"),
         }
 
+    def _parse_name(self, full_name: Optional[str]) -> Tuple[Union[str, None], Union[str, None]]:
+        """Parses the full name from Gitlab into the first and last name."""
+        if not full_name:
+            return None, None
+
+        name_parts = full_name.split()
+
+        if len(name_parts) == 1:
+            return name_parts[0], None
+
+        first_name = name_parts[0]
+        last_name = " ".join(name_parts[1:])
+        return first_name, last_name
+
     async def openid_from_response(self, response: dict, session: Optional["httpx.AsyncClient"] = None) -> OpenID:
+        """Converts Gitlab user info response to OpenID object."""
+        first_name, last_name = self._parse_name(response.get("name"))
+
         return OpenID(
             email=response["email"],
             provider=self.provider,
             id=str(response["id"]),
+            first_name=first_name,
+            last_name=last_name,
             display_name=response["username"],
             picture=response["avatar_url"],
         )
